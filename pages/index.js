f6465ffd7af61e23c04c9f5773265f21174ee132
Jorge Ferreiro <jorge@ferreiro.me>
2020-09-12 19:58:36 +0200

    Initial version of the app

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,75 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+import { Row, Col } from "antd";
+
+// Functional component
+function Tweet({ avatar, author, message, date, likesCount, comments }) {
+  return (
+    <Row
+      style={{
+        border: "1px solid #f0f0f0",
+        borderRadius: "10px",
+        padding: "1rem",
+      }}
+      gutter={24}
+    >
+      <Col flex="100px">
+        <img
+          src={avatar}
+          alt="User avatar"
+          style={{ width: "100%", borderRadius: "100%" }}
+        />
+      </Col>
+      <Col flex="auto">
+        <Row>{author}</Row>
+        <Row>{message}</Row>
+        TODO: Add the icons
+      </Col>
+    </Row>
+  );
+}
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Row>
+        <Col flex="100px">Menu</Col>
+        <Col flex={1}>
+          <Tweet
+            avatar="https://pbs.twimg.com/profile_images/1278619568508985344/MCS_SOvw_400x400.jpg"
+            author="Paul Martinez"
+            message="OMG! I'm enjoying this workshop a lot. Thanks for coming <3"
+            date={new Date()}
+            likesCount={10}
+            comments={[
+              {
+                author: "Paco",
+                message: "Keep it up!",
+              },
+            ]}
+          />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          <Tweet
+            avatar="https://pbs.twimg.com/profile_images/1278619568508985344/MCS_SOvw_400x400.jpg"
+            author="Jorge Ferreiro @jgferreiro"
+            message="OMG! I'm enjoying this workshop a lot. Thanks for coming <3"
+            date={new Date()}
+            likesCount={10}
+            comments={[
+              {
+                author: "Paco",
+                message: "Keep it up!",
+              },
+            ]}
+          />
+        </Col>
+        <Col flex={0}>Sidebar</Col>
+      </Row>
     </div>
-  )
+  );
 }
